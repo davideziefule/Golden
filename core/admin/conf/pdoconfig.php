9967a915d71dd2f407dc491e377d36336840f917
David Eziefule <62359843+davideziefule@users.noreply.github.com>
2024-02-03 23:21:08 +0100

    Update pdoconfig.php

--- core/admin/conf/pdoconfig.php
+++ core/admin/conf/pdoconfig.php
@@ -1,8 +1,8 @@
 <?php
-$DB_host = "localhost";
-$DB_user = "root";
-$DB_pass = "";
-$DB_name = "martdevelopers_iBanking";
+$DB_host = "ehc1u4pmphj917qf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$DB_user = "rcwpyucfd2g3b9zb";
+$DB_pass = "qte6tr3x37jikegu";
+$DB_name = "tgtgh4tdbn0259jb";
 try
 {
  $DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass);
