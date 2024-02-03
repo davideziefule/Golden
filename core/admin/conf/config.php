29239486f15b1cbf41b9a9ac91209be915132282
David Eziefule <62359843+davideziefule@users.noreply.github.com>
2024-02-03 23:03:50 +0100

    Update config.php

--- core/admin/conf/config.php
+++ core/admin/conf/config.php
@@ -1,6 +1,6 @@
 <?php
-    $dbuser="root";
-    $dbpass="";
-    $host="localhost";
-    $db="martdevelopers_iBanking";
+    $dbuser="rcwpyucfd2g3b9zb";
+    $dbpass="qte6tr3x37jikegu";
+    $host="ehc1u4pmphj917qf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $db="tgtgh4tdbn0259jb";
     $mysqli=new mysqli($host,$dbuser, $dbpass, $db);
