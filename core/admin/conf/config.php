11a672d9fb17cb9635bbdf56f14a459612af3a40
David Eziefule <62359843+davideziefule@users.noreply.github.com>
2024-02-04 12:05:26 +0100

    Update config.php

--- core/admin/conf/config.php
+++ core/admin/conf/config.php
@@ -1,6 +1,6 @@
 <?php
     $dbuser="rcwpyucfd2g3b9zb";
-    $dbpass="qte6tr3x37jikegu";
+    $dbpass="bwbsy4nog4ssm9cb";
     $host="ehc1u4pmphj917qf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
     $db="tgtgh4tdbn0259jb";
     $mysqli=new mysqli($host,$dbuser, $dbpass, $db);
